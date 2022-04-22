ca53ac49aac01e1f3bbe51176b7d2b8d711775eb
bws9000 <snyder.burt@gmail.com>
2022-04-22 08:43:31 -0400

    start basic factory/nft

--- contracts/misc/interface/IERC721Enumeration.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.9;
-
-interface ERC721Enumerable {
-    function totalSupply() external view returns (uint256);
-    function tokenByIndex(uint256 _index) external view returns (uint256);
-    function tokenOfOwnerByIndex(address _owner, uint256 _index) external view returns (uint256);
-}
