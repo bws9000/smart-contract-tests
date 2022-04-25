d2ffb69382b61b846d6bdd054d77cf56555754ad
Burt Snyder <snyder.burt@gmail.com>
2022-04-25 10:49:28 -0400

    update to super generic nft

--- contracts/BasicERC721.sol
+++ contracts/BasicERC721.sol
@@ -3,13 +3,9 @@ pragma solidity 0.8.9;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract BasicERC721 is ERC721, Ownable {
     
-    using Counters for Counters.Counter;
-    
-    Counters.Counter private _tokenIds;
     string public uri;
 
     constructor(
@@ -19,11 +15,5 @@ contract BasicERC721 is ERC721, Ownable {
         ERC721(_name,_symbol) {
             uri = _uri;
         }
-
-    function safeMint(address to) public onlyOwner {
-        uint256 tokenId = _tokenIds.current();
-        _tokenIds.increment();
-        _safeMint(to, tokenId);
-    }
     
 }
