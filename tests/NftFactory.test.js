ee80ed653f2c7c1a30cef14791ed74727a44dc40
bws9000 <snyder.burt@gmail.com>
2022-06-07 18:30:01 -0400

    added subscribe erc721 to factory

--- tests/NftFactory.test.js
+++ tests/NftFactory.test.js
@@ -1,4 +1,5 @@
 const { accounts, contract } = require('@openzeppelin/test-environment');
+const { expectRevert } = require('@openzeppelin/test-helpers');
 const { expect } = require('chai');
 
 describe('NftFactory', function () {
@@ -8,10 +9,12 @@ describe('NftFactory', function () {
   const NftFactory = contract.fromArtifact('NftFactory');
   const NftFactoryItemERC721 = contract.fromArtifact('NftFactoryItemERC721');
 
+
   beforeEach('Deploy NftFactory and NftFactoryItemERC721', async function () {
     this.nftFactory = await NftFactory.new({ from: owner });
     this.nftItem = await NftFactoryItemERC721.new({ from: owner });
   });
+
 
   describe('NftFactory Owner', function () {
     
@@ -21,9 +24,36 @@ describe('NftFactory', function () {
 
   });
 
-  describe('Spawn Nft with NftFactory', function () {
+
+
+  describe('Spawn Nft with NftFactory ( ERC721 not subscribed )', function () {
+
+      it('will revert', async function(){
+        
+        await expectRevert(
+          this.nftFactory.spawn(
+            this.nftItem.address, 
+            'MyNFT', 
+            'MNFT', 
+            'https://baseURI',
+            [],
+            { from: addr1 }),'Error: Address not subscribed'
+        );
+      });
+
+  });
+
+
+  describe('Spawn Nft with NftFactory ( subscribed )', function () {
 
       it('Spawn New Nft', async function(){
+        
+        // subscribe
+        const subReceipt = await this.nftFactory
+        .subscribeItem(this.nftItem.address, { from: owner });
+
+        expect(subReceipt.logs.filter(res => res.event === 'ItemSubscribed'));
+        
         const receipt = await this.nftFactory.spawn(
           this.nftItem.address, 
           'MyNFT', 
